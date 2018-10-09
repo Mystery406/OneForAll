4d1b2ce1baad8604453da89855b6aecaeb288cd0
Mystery406 <664117687@qq.com>
2018-10-09 17:33:40 +0800

    update

---
+++ app/src/main/java/com/dev/l/oneforall/App.java
@@ -0,0 +1,7 @@
+package com.dev.l.oneforall;
+
+import android.app.Application;
+
+public class App extends Application {
+
+}
---
+++ app/src/main/java/com/dev/l/oneforall/ui/BaseFragment.java
@@ -0,0 +1,7 @@
+package com.dev.l.oneforall.ui;
+
+import android.support.v4.app.Fragment;
+
+public class BaseFragment extends Fragment {
+
+}
